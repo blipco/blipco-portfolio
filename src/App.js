1129e7b8614facb9a0c89c798ffaad406eaa6493
Bobby <blipco619@gmail.com>
2018-07-11 19:38:50 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        My name is Bobby
+      </div>
+    );
+  }
+}
+
+const Header = () => {
+  return <header>I am a header</header>
+}
+export default App;
