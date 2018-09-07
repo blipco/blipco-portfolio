3ee634a2f36e9c7dd7499baa73dfec36281428cb
Bobby <blipco619@gmail.com>
2018-09-06 18:50:33 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
 
 class App extends Component {
   render() {
     return (
       <div>
-        My name is Bobby
+        My name is Bobby Pro
       </div>
     );
   }
-}
-
-const Header = () => {
-  return <header>I am a header</header>
 }
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = () => {
+  return <HeaderWrapper>I am a header</HeaderWrapper>
+}
+const HeaderWrapper = styled.header`
+height: 58px;
+background-color: yellow;
+`;
+
+export default Header;
