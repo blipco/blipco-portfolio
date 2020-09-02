616231a6e0526067e7289c9caabdc404cf37cc90
Bobby <blipco619@gmail.com>
2020-09-02 12:42:14 -0700

    update resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import bobby from '../assets/Bobby.png';
-import resume from '../assets/RESUME-8-12-19.pdf';
+import resume from '../assets/RESUME-8-29-20.pdf';
 
 class Navbar extends React.Component {
   
--- src/components/projects/MovieFinder.jsx
+++ src/components/projects/MovieFinder.jsx
@@ -12,14 +12,16 @@ class MovieFinder extends React.Component {
           <p>
             The Movie Finder allows the user to search for a movie by the title.
             It shows 10 search results at a time.  It also provides the user with
-            more information about the movie like IMDB rating, description, 
+            more information about the movie like IMDB rating, description,
+            and movie poster.
           </p>
           <h3>
             <strong>What I Used</strong>
           </h3>
           <p>
-            My portfolio is created with Javascript, React and React Router. <br/>
-            I designed it with custom CSS styling.
+            The Movie Finder was created with Javascript, React and React Router. <br/>
+            In order to render the movie information I used the OMDB API.
+                the OMDB API. I designed it with custom CSS styling.
           </p>
           <a href="https://github.com/blipco/startnow-react200-movie-finder" id="movieFinderLink" className="projectLinks" target="_blank">
             GitHub Repository
